d2d7521f72bcc404f1f06a3ec76c3dc6c158fd56
MUXICORP\lgama <lgama@muxi.com.br>
2022-06-23 19:50:53 -0300

    Removing UserResponse and changing for Call

--- app/src/main/java/com/picpay/desafio/android/service/PicPayService.kt
+++ app/src/main/java/com/picpay/desafio/android/service/PicPayService.kt
@@ -1,15 +1,12 @@
 package com.picpay.desafio.android.service
 
 import com.picpay.desafio.android.data.model.UserModel
-import com.picpay.desafio.android.data.model.UserResponse
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.GET
 
 
 interface PicPayService {
 
     @GET("users")
-    suspend fun getUsers(): Response<UserResponse>
-//    fun getUsers(): Call<List<UserModel>>
+    fun getUsers(): Call<List<UserModel>>
 }
