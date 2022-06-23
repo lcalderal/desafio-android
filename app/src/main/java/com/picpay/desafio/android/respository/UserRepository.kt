5e2718f941bdf9af8f5764cef058c361dfb285c3
MUXICORP\lgama <lgama@muxi.com.br>
2022-06-23 19:49:41 -0300

    removing the suspend

--- app/src/main/java/com/picpay/desafio/android/respository/UserRepository.kt
+++ app/src/main/java/com/picpay/desafio/android/respository/UserRepository.kt
@@ -4,5 +4,5 @@ import com.picpay.desafio.android.service.PicPayService
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(private val api: PicPayService) {
-    suspend fun getUsers() = api.getUsers()
+    fun getUsers() = api.getUsers()
 }
