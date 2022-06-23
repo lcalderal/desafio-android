9cc340a0b03642743275bb5e7059b1105c2c40f6
MUXICORP\lgama <lgama@muxi.com.br>
2022-06-23 11:33:59 -0300

    Change the getUsers() to return Response

--- app/src/main/java/com/picpay/desafio/android/service/PicPayService.kt
+++ app/src/main/java/com/picpay/desafio/android/service/PicPayService.kt
@@ -1,12 +1,15 @@
 package com.picpay.desafio.android.service
 
 import com.picpay.desafio.android.data.model.UserModel
+import com.picpay.desafio.android.data.model.UserResponse
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
 
 interface PicPayService {
 
     @GET("users")
-    fun getUsers(): Call<List<UserModel>>
+    suspend fun getUsers(): Response<UserResponse>
+//    fun getUsers(): Call<List<UserModel>>
 }
