f462ee35a32e6da98a4f0b8d64bfa5701705899d
MUXICORP\lgama <lgama@muxi.com.br>
2022-06-25 15:57:42 -0300

    removing unnecessary imports

--- app/src/main/java/com/picpay/desafio/android/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/MainActivity.kt
@@ -1,26 +1,9 @@
 package com.picpay.desafio.android
 
 import android.os.Bundle
-import android.view.View
-import android.widget.ProgressBar
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.picpay.desafio.android.adapter.UserListAdapter
-import com.picpay.desafio.android.data.model.UserModel
 import com.picpay.desafio.android.databinding.ActivityMainBinding
-import com.picpay.desafio.android.service.PicPayService
 import dagger.hilt.android.AndroidEntryPoint
-import okhttp3.OkHttpClient
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
